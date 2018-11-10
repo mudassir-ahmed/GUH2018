97c5f97f0b39dcc9298382f4f290dc8cb896c750
Mudassir Ahmed <mudassirahmedbashir@hotmail.co.uk>
2018-11-10 15:16:42 +0000

    add timeline base code

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -4,46 +4,60 @@ $( document ).ready(function( ) {
   // Just so we know jQuery is working in timeline.js
   console.log("asset: timeline.js is running with jquery");
 
-  // object creation
-  var htmlTimelineEntity = {
-    index: undefined,
-    animation_type: undefined, // fly or fade
-    animation_direction: undefined, // top, bottom, left, right
-    x_value: undefined, // x-value of position
-    y_value: undefined, // y-value of position
-    year: undefined, // we need this to work out when elements show/hide
-    layer: undefined, // just to see what is on what - may be used
-    set: {
-      animation: function(animation_in, animation_out, animation_direction) {
-        this.animation_type = animation_type;
-        this.animation_direction = animation_direction;
-      },
-      coordinates: function(x_value, y_value) {
-        this.x_value = x_value;
-        this.y_value = y_value;
-      },
-      year: function(year) {
-        this.year = year;
-      },
-      layer: function(layer) {
-        this.layer = layer;
-      }
-    }
+  // object constructor
+  function htmlTimelineEntity(index, animation_type, animation_direction, x_value, y_value, layer, year) {
+    this.index = index;
+    this.animation_type = animation_type;
+    this.animation_direction = animation_direction;
+    this.x_value = x_value;
+    this.y_value = y_value;
+    this.layer = layer;
+    this.year = year;
   };
 
   // empty array to store historic elements that animate
   var historicElements = [];
 
   // scan through each element and create a historic object
-  $( "html-historic-element" ).each(function( ) {
+  // docs for $().each() at https://api.jquery.com/each/
+  $( ".html-historic-element" ).each(function( index ) {
 
-    // create an object for each element to be used later
-    // and add it to the array.
+    console.log("RUNNUNG LOOp");
+
+    // access the html element
+    var historic_el = $( ".html-historic-element" ).index( index );
+
+    console.log(historic_el);
+
+    // populate this with the information.
+
+    // sets the animation porperties to be used later
+    var animation_type = $( this ).attr( "animation-type" );
+    var animation_direction = $( this ).attr( "animation-direction" );
+
+    // sets the coordinates - not needed at the moment
+    var x_value = $( this ).attr( "x-value" );
+    var y_value = $( this ).attr( "y-value" );
+
+    // sets the layer - not needed at the moment
+    var layer = $( this ).attr( "layer" );
+
+    // sets the year
+    var year = $( this ).attr( "year" );
+
+    // create an object for each element to be used later.
+    historicElementItem = new htmlTimelineEntity(index, animation_type, animation_direction, x_value, y_value, layer, year);
+
+    // add this object to the array.
+    historicElements.push( historicElementItem );
+
+    // DEBUG: checking if object is made
+    console.log( historicElementItem );
 
   });
 
   // let's see if we've saved this data properly
-  console.log();
+  console.log( historicElements );
 
 
 });
