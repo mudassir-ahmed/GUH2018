12af7c80d8f73c2139536fbeffa4732b5151f287
Mudassir Ahmed <mudassirahmedbashir@hotmail.co.uk>
2018-11-11 02:57:54 +0000

    animation based on year working

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,3 +1,5 @@
+
+
 // All javascript for timeline features appear here
 $( document ).ready(function( ) {
 
@@ -22,12 +24,12 @@ $( document ).ready(function( ) {
   // docs for $().each() at https://api.jquery.com/each/
   $( ".html-historic-element" ).each(function( index ) {
 
-    console.log("RUNNUNG LOOp");
+    //console.log("RUNNUNG LOOp");
 
     // access the html element
     var historic_el = $( ".html-historic-element" ).index( index );
 
-    console.log(historic_el);
+    //console.log(historic_el);
 
     // populate this with the information.
 
@@ -52,12 +54,125 @@ $( document ).ready(function( ) {
     historicElements.push( historicElementItem );
 
     // DEBUG: checking if object is made
-    console.log( historicElementItem );
+    // console.log( historicElementItem );
 
   });
 
   // let's see if we've saved this data properly
-  console.log( historicElements );
+  // console.log( historicElements );
+
+  // lets get the height of the browser window frame body
+  var browserViewportHeight = $("#ie-window-frame-body").height();
+
+  // console.log("fake viewport height dsfdsf: " + browserViewportHeight);
+
+  // this is the element that gets scrolled to see the the different years
+  var timelineScrollableElement = $("#ie-window-frame-body-inner");
+
+  var scrollPercentagePrevious = undefined;
+
+  timelineScrollableElement.scroll(function() {
+
+    var scrollHeight = document.getElementById("ie-window-frame-body-inner").scrollHeight;
+
+    // scroll amount between 0 and 1
+    var scrollPercentage = ( timelineScrollableElement.scrollTop() / ( scrollHeight - timelineScrollableElement.height() ) );
+
+    if( scrollPercentagePrevious != undefined ) {
+
+      var scrollDirection = scrollPercentagePrevious > scrollPercentage ? "past" : "future";
+
+      // console.log(scrollDirection);
+
+    }
+
+    // updates each time scroll event fired
+    scrollPercentagePrevious = scrollPercentage;
+
+    // update current year
+    // base year + (percentage scrolled * years to current date)
+    // gives you the current time travelled year
+    var currentYear = 1990 + Math.round( (2018 - 1990) * scrollPercentage );
+    $( "#ie-window-frame-current-year-content" ).text( currentYear );
+
+    // lets morph the page layout/design depending on what the year is
+    // and what direction in time you are travelling (past or future)
+
+    if ( scrollDirection == "past" ) {
+
+      //console.log("travelling to past")
+
+    }
+
+    if ( scrollDirection == "future" ) {
+
+      //console.log("travelling to future");
+
+    }
+
+    addCSSClassesToElementsBasedOnYear( currentYear, historicElements ); // either 1990, 2000, 2018
+
+  });
+
+  // get rid of elements that are not of this year upon start up
+  // starts at 1990
+  addCSSClassesToElementsBasedOnYear( 1990, historicElements );
+
+  function addCSSClassesToElementsBasedOnYear( currentYear, historicElements) {
+    // only one condition can be true in one point in time
+    if ( currentYear == 1990 || currentYear == 2000 || currentYear == 2018 ) {
+
+      for (var i = 0; i < historicElements.length; i++) {
+
+        var historicItem = historicElements[i];
+
+        if( historicItem.year == currentYear ) {
+
+          morphHTMLElement(historicItem, true);
+
+          console.log("TRY TO SHOW: " + historicItem.year);
+
+        } else {
+
+          morphHTMLElement(historicItem, false);
+
+          console.log("TRY TO HIDE: " + historicItem.year);
+
+        }
+
+      }
+
+    }
+  }
+
+  function morphHTMLElement(historicElementObject, show) {
+
+    //console.log(historicElementObject);
+
+    // class made in the following syntax
+    // html-historic-element-animate-<transition_style>-<timeline_direction>-<direction_if_appicable>
+
+    var showHide = show ? "show" : "hide";
+
+    var className = "html-historic-element-animate-" + historicElementObject.animation_type + "-" + showHide;
+
+    var invertShowHide = !show ? "show" : "hide";
+
+    var invertClassName = "html-historic-element-animate-" + historicElementObject.animation_type + "-" + invertShowHide;
+
+    console.log(historicElementObject.year + " needs class " + className)
+
+    if (historicElementObject.animation_type != "fade") {
+
+      // add animate direction to class name
+
+    }
+
+    // https://api.jquery.com/eq-selector/
+    $( ".html-historic-element:eq(" + historicElementObject.index + ")" ).removeClass( invertClassName );
+    $( ".html-historic-element:eq(" + historicElementObject.index + ")" ).addClass( className );
+
+  }
 
 
 });
